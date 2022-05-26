bcebd431f7dcbe18cebf217665953beec71f6e84
IGPI-2000060 <yuki.sakurai@ignitionpoint-inc.com>
2022-05-26 13:11:59 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "csfindermockvue": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
